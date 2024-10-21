5105413e6978abf26a564cf05ca8d1418af3e374
bojanalukic99 <lukicb99@gmail.com>
2024-10-21 19:14:42 +0200

    init

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio!</h1>
+            <p>This is the homepage built with JavaScript.</p>
+        </div>
+    );
+}
